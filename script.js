445ed259bc7afe0f258053624598727f950142a9
Zaino <hello@darrin.dev>
2021-03-26 15:15:14 -0700

    add feature: password generates

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
+// * create the values to use in the randomly generated password  
+var empty = "";
+var uCaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lCaseValues = "abcdefghijklmnopqrstuvwxyz";
+var numberValues = "0123456789";
+var symbolValues = "!@#$%^&*()<>?";
 
-//when the user clicks the button prompt them to select criteria
-//display the following criteria to the user:
-//criteria: number of characters from 8-128 
-//check: if below 8 or above 128 ask the user to try again
-//criteria: include lowercase true/false
-//criteria: include uppercase true/false
-//criteria: include special characters true/false
-//at least one character type should be selected 
-//if all required conditions are met generate a random password 
-//for all true criteria values include the character type in the randomly generated password
-//display the password to the user 
+//* Declare user confirm variables 
+var pLength  ; 
+var numbers = false;
+var upperCase = false;
+var lowerCase = false;
+var symbols = false;
 
 
-// Assignment Code
+//* Assignment Code: when the user clicks the button prompt them to select criteria
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// * Assignment Code: Add event listener to the generate button
+generateBtn.addEventListener("click", writePassword);
+
+//* ask the user to confirm the criteria using window.prompt 
+// ! not sure this is working - see console.log below 
+function generatePassword() {
+  // *the user inputs the length of the password 
+  pLength = window.prompt("Please enter a number between 8-128 characters to set password length");
+  while (pLength < 8 || pLength > 128 ) { 
+      pLength = window.prompt("Please try again with a number between 8 - 128"); 
+  }
+  //*the user inputs either ok = true or cancel = false for these values 
+  numbers = window.confirm("Click OK to include numbers in your password. Otherwise click cancel.");
+  upperCase = window.confirm("Click OK to include UPPER CASE letters in your password. Otherwise click cancel.");
+  lowerCase = window.confirm("Click OK to include lower case letters in your password. Otherwise click cancel.");
+  symbols = window.confirm("Click OK to include symbols in your password. Otherwise click cancel.");
+  var bank = "";
+if (numbers) {
+  bank += numberValues;
+} 
+if (upperCase) {
+  bank += uCaseValues;
+}
+if (lowerCase) {
+  bank += lCaseValues;
+}
+if (symbols) {
+  bank += symbolValues;
+}
+return randomize(bank);
+
+};
+
+
+//* Loop through the value variables to generate a random password
+//* vault equals an empty variable into which we are going to put our password variables
+//* the loop takes vault as an empty space and iterates through the boolean values for the length of pLength
+//* if a value is positive it is added to the vault 
+//* vault then randomizes the numbers using math floor and random 
+//* when we return vault it is ready to pass the values to password 
+function randomize(bank) {
+ var vault = "";
+ for (i = 0; i < pLength; i++) {
+  // loop thru bank and pull characters 
+  // += keeps what was in the value before and adds 
+  vault += bank[Math.floor(Math.random() * bank.length)]
+}
+console.log(vault);
+return vault;
+}; 
+
+
+// todo vault should pass the value of the password to below
+
+var vault = password;
+
+// * Assignment Code: Write password to the #password input
+// * This is going to write to the form once the password has been generated 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -24,19 +82,10 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//*let's check the values confirmed by user 
 
-function generatePassword() {
-  var passLength = window.prompt("Please choose a password between 8 -128 characters");
-  while (passLength < 8 || passLength > 128 ) { 
-      passLength = window.prompt("Please try again with a number between 8 - 128"); 
-  }
-  var upperCase = window.confirm("Would you like to include UPPER CASE letters?");
-  var lowerCase = window.confirm("Would you like to include lower case letters?");
-  var sppecialChars = window.confirm("Would you like to include special characters?");
-}
-
-// use a loop to iterate thru and add our values in the variables 
-// use a for loop ?
-// use if statements 
+console.log(pLength);// a number between 8-128
+console.log(numbers); //true/false
+console.log(upperCase);//true/false
+console.log(lowerCase);//true/false
+console.log(symbols);//true/false

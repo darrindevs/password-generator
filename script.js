77bdbe080b5da5289e30dae25ac70b1d8b848f44
Zaino <hello@darrin.dev>
2021-03-25 17:23:20 -0700

    add start of script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+//when the user clicks the button prompt them to select criteria
+//display the following criteria to the user:
+//criteria: number of characters from 8-128 
+//check: if below 8 or above 128 ask the user to try again
+//criteria: include lowercase true/false
+//criteria: include uppercase true/false
+//criteria: include special characters true/false
+//at least one character type should be selected 
+//if all required conditions are met generate a random password 
+//for all true criteria values include the character type in the randomly generated password
+//display the password to the user 
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +26,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passLength = window.prompt("Please choose a password between 8 -128 characters");
+  while (passLength < 8 || passLength > 128 ) { 
+      passLength = window.prompt("Please try again with a number between 8 - 128"); 
+  }
+  var upperCase = window.confirm("Would you like to include UPPER CASE letters?");
+  var lowerCase = window.confirm("Would you like to include lower case letters?");
+  var sppecialChars = window.confirm("Would you like to include special characters?");
+}
+
+// use a loop to iterate thru and add our values in the variables 
+// use a for loop ?
+// use if statements 
